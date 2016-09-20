2505d08d95cff30b5d17cc6c40d06c97e174ea59
martin <railific@gmail.com>
2016-09-20 20:37:21 +0700

    convert to postgresql

--- db/migrate/20160916080859_devise_create_users.rb
+++ db/migrate/20160916080859_devise_create_users.rb
@@ -39,13 +39,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
 
-    ### My stuff
-    # Initialize first account:
-    User.create! do |u|
-        u.email     = 'test@test.com'
-        u.password    = 'password'
-    end
-    ### End My stuff
 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160916111225) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "prefs", force: :cascade do |t|
     t.integer  "language"
     t.string   "timezone"
@@ -20,7 +23,7 @@ ActiveRecord::Schema.define(version: 20160916111225) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["user_id"], name: "index_prefs_on_user_id"
+    t.index ["user_id"], name: "index_prefs_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,8 +39,8 @@ ActiveRecord::Schema.define(version: 20160916111225) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
